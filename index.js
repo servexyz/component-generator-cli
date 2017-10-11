84225f115fa8c6ee58f2171b01a56ef747ec01f9
alechp <me@alechp.com>
2017-10-11 13:33:18 -0700

    Created extractor portion of parseFormat
    * Added examples to show how you can use the format.json config file that's provided or how to use strings

--- index.js
+++ index.js
@@ -8,17 +8,43 @@ const log = console.log;
 /*
  Procedural Steps;
  1. [optional] setFormat()
- 2. [required] parseFormat(format)
- 3.
+ 2. [required] parseFormat(format.structure, format.extensions)
+ 3. [required] componentFactory => componentGenerator => directory/files*
 */
 
-const format = path.join(__dirname, '/format.json');
+var format = path.join(__dirname, '/format.json');
 
-function setFormat() {
-
+function getFormatPath(configFilePath: string): string {
+  let cfp = configFilePath;
+  if(!(empty(cfp))){
+    format = cfp;
+  }
+  return format;
 }
-function parseFormat() {
 
+/*
+------------------------------------------
+The following two examples are equivalent
+------------------------------------------
+Manual (without format.json):
+  @@structure: ["Component.$js, Component.test.js", "index.js"]
+  @@extensions: { "$js": "js", "$css": "scss"}
+
+Standard (with format.json):
+  @@structure: "solo"
+  @@extensions:  "js-scss"
+*/
+function parseFormat(structure: Array<string> | string, extensions: Array<string> | string): Array<string> {
+  if((typeof structure === "string") && (typeof extensions === "string")) {
+    let formatFile = getFormatPath();
+    fs.readFile('file', 'utf8', (err, data) => {
+      var formatObject = data;
+    });
+    var formatStructure = formatObject.structure;
+    var formatExtensions = formatObject.extensions;
+  }
+  for (const [key, value] of Object.entries(formatStructure)) {
+  }
 }
 
 function fileFactory(
@@ -26,9 +52,7 @@ function fileFactory(
   directoryNames: Array<string>
 ) {
   let locale = path.join(process.cwd(), rootCreationLocation);
-  // if (empty(textObject)) {
-  // } else {
-  // }
+
 }
 fileFactory();
 /*
