bdbc046670c151edc47d5c9d51f561b40a5f0b57
alechp <me@alechp.com>
2017-10-17 12:48:42 -0700

    createFiles method and more added.

--- dist/factor.js
+++ dist/factor.js
@@ -1,10 +1,66 @@
 "use strict";
 
 const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const chalk = require("chalk");
 const log = console.log;
 
 function factory(components) {
   log(`Components being passed: ${components}`);
+  components.map(c => {
+    let dir = createDirectory(c);
+    try {
+      let files = createFiles(c, dir);
+      return files;
+    } catch (error) {
+      console.error(`${chalk.red("createFiles in factory failed.")} ${error}`);
+    }
+  });
+}
+function createDirectory(component) {
+  let flag = null;
+  mkdirp(component, err => {
+    err ? flag = false : flag = true;
+  });
+  if (flag == true) {
+    return component; //return name of component which will be used as directory
+  }
+  return flag; //if null or false
+}
+function createFile(component) {
+  fs.writeFile(component, "", "utf-8", error => {
+    error ? console.error(`createFile() failed. ${error}`) : log(`${component} was created`);
+  });
+}
+function createFiles(component, directory) {
+  var formatConfig = path.join(__dirname, "/format.json");
+  var formatObject = require(formatConfig);
+  log(`formatObject: ${JSON.stringify(formatObject)}`);
+  var structure = formatObject.structure;
+  let templatedFileNames = grabValueOfKeyFromObject("structure", structure);
+  log(`\n\nTemplatedFileNames: ${templatedFileNames}`);
+  log("\n\nStructure: " + JSON.stringify(structure));
+  if (structure) {
+    let files = templatedFileNames.map(tfn => {
+      let file = tfn.replace(/([A-Z])\w+/, component);
+      let here = path.join(process.cwd(), directory, file);
+      log(`${chalk.green("Here: " + here)}`);
+      return createFile(here);
+    });
+    return files;
+  } else {
+    console.log(`${chalk.red("structure isn't defined. Current value: ")} ${structure}`);
+    return false;
+  }
+}
+
+function grabValueOfKeyFromObject(key, obj) {
+  for (const [k, v] of Object.entries(obj)) {
+    if (k == key) {
+      return v;
+    }
+  }
 }
 
 module.exports = factory;
--- src/factor.js
+++ src/factor.js
@@ -1,8 +1,70 @@
 const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const chalk = require("chalk");
 const log = console.log;
 
 function factory(components) {
   log(`Components being passed: ${components}`);
+  components.map(c => {
+    let dir = createDirectory(c);
+    try {
+      let files: Array<string> = createFiles(c, dir);
+      return files;
+    } catch (error) {
+      console.error(`${chalk.red("createFiles in factory failed.")} ${error}`);
+    }
+  });
+}
+function createDirectory(component: string) {
+  let flag: boolean = null;
+  mkdirp(component, err => {
+    err ? (flag = false) : (flag = true);
+  });
+  if (flag == true) {
+    return component; //return name of component which will be used as directory
+  }
+  return flag; //if null or false
+}
+function createFile(component: string) {
+  fs.writeFile(component, "", "utf-8", error => {
+    error
+      ? console.error(`createFile() failed. ${error}`)
+      : log(`${component} was created`);
+  });
+}
+function createFiles(component, directory) {
+  var formatConfig: string = path.join(__dirname, "/format.json");
+  var formatObject: Object = require(formatConfig);
+  var structure: Array<string> = formatObject.structure;
+  let templatedFileNames = grabValueOfKeyFromObject("structure", structure);
+  log(`\n\nTemplatedFileNames: ${templatedFileNames}`);
+  log("\n\nStructure: " + JSON.stringify(structure));
+  if (structure) {
+    let files = templatedFileNames.map(tfn => {
+      let file = tfn.replace(/([A-Z])\w+/, component);
+      let here = path.join(process.cwd(), directory, file);
+      log(`${chalk.green("Here: " + here)}`);
+      return createFile(here);
+    });
+    return files;
+  } else {
+    console.log(
+      `${chalk.red("structure isn't defined. Current value: ")} ${structure}`
+    );
+    return false;
+  }
+}
+
+function grabValueOfKeyFromObject(
+  key: string,
+  obj: mixed
+): Array<string> | mixed {
+  for (const [k, v] of Object.entries(obj)) {
+    if (k == key) {
+      return v;
+    }
+  }
 }
 
 module.exports = factory;
