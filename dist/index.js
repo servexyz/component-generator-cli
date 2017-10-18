879e25245199e67ae3a5ca2af7025dfc430b5d6c
alechp <me@alechp.com>
2017-10-17 21:15:41 -0700

    Updated factory name to generator.js

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,9 @@
 const generator = require("./generator");
 const vorpal = require("vorpal")();
 
-vorpal
-  .command("m [components...]", "Creates 1 or more component directories")
-  .action(function(args, cb) {
-    generator(args.components);
-    cb();
-  });
+vorpal.command("m [components...]", "Creates 1 or more component directories").action(function (args, cb) {
+  generator(args.components);
+  cb();
+});
 
 vorpal.delimiter("component-generator-cli$").show();
