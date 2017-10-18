90e603c6a0c2471f05c60296abf67d10d6355e80
alechp <me@alechp.com>
2017-10-17 18:52:27 -0700

    Added bash code at top of file so it can be executed (hopefully)

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,11 @@
 const factory = require("./factory");
 const vorpal = require("vorpal")();
 
-vorpal.command("m [components...]", "Creates 1 or more component directories").action(function (args, cb) {
-  factory(args.components);
-  cb();
-});
+vorpal
+  .command("m [components...]", "Creates 1 or more component directories")
+  .action(function(args, cb) {
+    factory(args.components);
+    cb();
+  });
 
 vorpal.delimiter("component-factory-cli$").show();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // @flow
 
 const factory = require("./factory");
