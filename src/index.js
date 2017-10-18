51018f6196c8e226fedb5957114d6fce034df213
alechp <me@alechp.com>
2017-10-17 17:25:00 -0700

    Chiiiianges
    * cleaned up readme
    * added main.js script to package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const factory = require("./factory");
 const vorpal = require("vorpal")();
+
 vorpal
   .command("m [components...]", "Creates 1 or more component directories")
   .action(function(args, cb) {
